1850f47c7dddb007c450b5278375783c6e5ba456
unknown <seuemailaqui>
2025-06-04 22:38:02 -0300

    adicionadas classes de security

---
+++ src/main/java/com/borborema/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.borborema.usuario.infrastructure.repository;
+
+import com.borborema.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
